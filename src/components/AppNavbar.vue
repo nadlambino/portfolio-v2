a4a957b5a8d065dee9507f4bfb489e55a3e6e7f9
Nad Lambino <50892397+nadlambino@users.noreply.github.com>
2023-07-12 22:14:56 +0800

    Fix typo

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -7,7 +7,7 @@
             <li><a href="#home">Home</a></li>
             <li><a href="#about">About</a></li>
             <li><a href="#skills">Skills</a></li>
-            <li><a href="#projects">Porjects</a></li>
+            <li><a href="#projects">Projects</a></li>
         </ul>
     </nav>
 </template>
