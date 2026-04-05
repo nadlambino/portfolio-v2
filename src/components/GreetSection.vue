0ac7d932079f5536034ba864854f9a5b5a72ec34
Nad Lambino <ronaldlambino0000@gmail.com>
2026-04-05 10:06:40 +0000

    set default theme to light mode for deployment verification

--- src/components/GreetSection.vue
+++ src/components/GreetSection.vue
@@ -17,7 +17,7 @@ const navElement = ref(null)
 const { isVisible } = useObserver(navElement, true, '.body-navbar', {
   rootMargin: '-60px 0px 0px 0px'
 })
-const mode = useStorage('mode', 'dark')
+const mode = useStorage('mode', 'light')
 const parts = reactive(particles)
 
 watch(
